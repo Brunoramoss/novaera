20964b760327aa81c1ab5c74b175bf45c4fda6de
Bruno ramos <ramosbruno1299@gmail.com>
2023-06-30 15:47:21 -0300

    add readme

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 function toggleMode() 
 {
     let img = document.getElementById('profile-img');
+    let altDark= "foto de bruno ramos com sol batendo no rosto com fundo de praia";
+    let altLight = " Foto de Bruno Ramos Sorrindo";
     const html = document.documentElement
     html.classList.toggle('light');
-    img.setAttribute("src", html.classList.contains("light") ? "./imagem/bruno-light.jpeg" : "./imagem/bruno ramos.jpg");
-    /*if(html.classList.contains('light'))
+    img.setAttribute("src", html.classList.contains("light") ? "./imagem/Bruno-light.jpeg" : "./imagem/bruno ramos.jpg");
+    img.setAttribute("alt",  html.classList.contains("light") ? altLight : altDark);
+
+
+ /*if(html.classList.contains('light'))
     {
         html.classList.remove('light')
     }
